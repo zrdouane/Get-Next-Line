6a67b17b1a25dd53d98e0a3218ef32815a6d5bd5
Call me zack <85793706+zrdouane@users.noreply.github.com>
2022-02-18 22:17:32 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zrdouane <zrdouane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 17:12:21 by zrdouane          #+#    #+#             */
+/*   Updated: 2022/01/09 01:44:17 by zrdouane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_read(int fd, char *buffers)
+{
+	char	calcul[BUFFER_SIZE + 1];
+	int		byte;
+
+	byte = 1;
+	while (!ft_strchr(buffers, '\n') && byte)
+	{
+		byte = read(fd, calcul, BUFFER_SIZE);
+		if (byte == -1)
+			return (0);
+		calcul[byte] = '\0';
+		buffers = ft_strjoin(buffers, calcul);
+	}
+	return (buffers);
+}
+
+char	*ft_readline(char *buffers)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	if (!buffers[i])
+		return (0);
+	while (buffers[i] != '\n' && buffers[i] != '\0')
+		i++;
+	if (buffers[i] == '\n')
+		i++;
+	tmp = (char *)malloc(sizeof(char) * i + 1);
+	if (!tmp)
+		return (0);
+	i = 0;
+	while (buffers[i] != '\n' && buffers[i] != '\0')
+	{
+		tmp[i] = buffers[i];
+		i++;
+	}
+	if (buffers[i] == '\n')
+		tmp[i++] = '\n';
+	tmp[i] = '\0';
+	return (tmp);
+}
+
+char	*ft_rest(char *buffers)
+{
+	int		i;
+	char	*rmn;
+	int		len;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (buffers[i] != '\n' && buffers[i])
+		i++;
+	if (!buffers[i])
+	{
+		free(buffers);
+		return (0);
+	}
+	len = ft_strlen(buffers) - i;
+	rmn = (char *)malloc(sizeof(char) * len + 1);
+	if (!rmn)
+		return (0);
+	i++;
+	while (buffers[i])
+		rmn[j++] = buffers[i++];
+	rmn[j] = '\0';
+	free(buffers);
+	return (rmn);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffers;
+	char		*line;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (0);
+	buffers = ft_read(fd, buffers);
+	if (!buffers)
+		return (0);
+	line = ft_readline(buffers);
+	buffers = ft_rest(buffers);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zrdouane <zrdouane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 17:12:44 by zrdouane          #+#    #+#             */
+/*   Updated: 2022/01/09 01:13:58 by macbookai        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+char	*ft_read(int fd, char *buffers);
+char	*ft_readline(char *buffers);
+char	*ft_rest(char *buffers);
+char	*get_next_line(int fd);
+
+#endif
